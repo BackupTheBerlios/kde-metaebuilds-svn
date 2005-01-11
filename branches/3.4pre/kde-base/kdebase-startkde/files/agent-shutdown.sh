b1b8104195a093f9e73b0ef93218a330c9566d13
danarmak <danarmak@f8797452-06e6-0310-882f-e560811728ff>
2005-01-11 20:54:04 +0000

    apply fix for gpg-agent shutdown from bug 76720

--- branches/3.4pre/kde-base/kdebase-startkde/files/agent-shutdown.sh
+++ branches/3.4pre/kde-base/kdebase-startkde/files/agent-shutdown.sh
@@ -2,8 +2,8 @@
 #
 # Customized agents shutdown file
 #
-#if [ -x /usr/bin/gpg-agent ]; then
-#  eval "$(/usr/bin/gpg-agent -k)"
+#if [ -x /usr/bin/gpg-agent -a -n "$GPG_AGENT_INFO" ]; then
+#  kill $(echo ${GPG_AGENT_INFO} | cut -d':' -f 2) 2>&1 >/dev/null
 #fi 
 #
 #if [ -x /usr/bin/ssh-agent ]; then
